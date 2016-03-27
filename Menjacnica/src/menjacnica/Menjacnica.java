417c13623d0914e94e680c920e333da1cf32cf0c
marinaguzvic <marina.guzvic@gmail.com>
2016-03-27 16:06:05 +0200

    Klasa Menjacnica implementira interfejs MenjacnicaInterfejs, dodate su joj prazne metode, neimplementirane

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -3,10 +3,30 @@ package menjacnica;
 import java.util.Date;
 import java.util.LinkedList;
 
+import interfejsi.MenjacnicaInterfejs;
 import valute.Kurs;
+import valute.Valuta;
 
-public class Menjacnica {
+public class Menjacnica implements MenjacnicaInterfejs{
 	LinkedList<KurseviNaOdredjeniDan> dani = new LinkedList<>();
+
+	@Override
+	public void dodajKurs(Kurs kurs, Date dan) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbrisiKurs(Kurs kurs, Date dan) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Kurs vratiKurs(Valuta valuta1, Valuta valuta2, Date dan) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	
 	
